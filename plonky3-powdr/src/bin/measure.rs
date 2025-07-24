e7cf34e1edabe1f0edc49d7d78b50049c1c60eae
guorong009 <dgr009@pse.dev>
2025-07-24 16:20:56 +0800

    feat: replace "memory_stats" with peak memory measure in plonky3-powdr

--- plonky3-powdr/src/bin/measure.rs
+++ plonky3-powdr/src/bin/measure.rs
@@ -5,30 +5,21 @@ use tikv_jemallocator::Jemalloc;
 #[cfg(not(target_env = "msvc"))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
-
-use memory_stats::memory_stats;
+use utils::bench::measure_peak_memory;
 
 fn main() {
-    let usage_before = memory_stats().unwrap();
+    let (mut pipeline, peak_memory) = measure_peak_memory(|| prepare_pipeline());
 
-    let mut pipeline = prepare_pipeline();
-
-    let usage_after = memory_stats().unwrap();
     println!(
-        "Preprocessing memory usage: {} GB resident | {} GB virt",
-        (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0 * 1024.0),
-        (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0 * 1024.0)
+        "Preprocessing peak memory: {} GB",
+        peak_memory as f32 / (1024.0 * 1024.0 * 1024.0),
     );
 
-    let usage_before = memory_stats().unwrap();
+    let (_, peak_memory) = measure_peak_memory(|| prove(&mut pipeline));
 
-    prove(&mut pipeline);
-
-    let usage_after = memory_stats().unwrap();
     println!(
-        "Proving memory usage: {} GB resident | {} GB virt",
-        (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0 * 1024.0),
-        (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0 * 1024.0)
+        "Proving peak memory: {} GB",
+        peak_memory as f32 / (1024.0 * 1024.0 * 1024.0),
     );
 
     verify(pipeline);
