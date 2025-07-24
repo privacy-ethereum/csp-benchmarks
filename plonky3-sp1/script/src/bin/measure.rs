2551e72b0d34fad854be17c9c2509172651daaa8
guorong009 <dgr009@pse.dev>
2025-07-24 16:30:17 +0800

    feat: replace "memory_stats" with peak memory measure in plonky3-sp1

--- plonky3-sp1/script/src/bin/measure.rs
+++ plonky3-sp1/script/src/bin/measure.rs
@@ -1,5 +1,5 @@
-use memory_stats::memory_stats;
 use sp1_sdk::{ProverClient, SP1Stdin, include_elf};
+use utils::bench::measure_peak_memory;
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
 pub const SHA_ELF: &[u8] = include_elf!("sha-program");
@@ -9,35 +9,28 @@ fn main() {
     let client = ProverClient::from_env();
     let stdin = SP1Stdin::new();
 
-    let usage_before = memory_stats().unwrap();
-
     // Setup the program for proving.
-    let (_, _) = client.setup(SHA_ELF);
-
-    let usage_after = memory_stats().unwrap();
+    let ((_, _), peak_memory) = measure_peak_memory(|| client.setup(SHA_ELF));
 
     println!(
-        "Preprocessing memory usage: {} GB resident | {} GB virt",
-        (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0 * 1024.0),
-        (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0 * 1024.0)
+        "Preprocessing peak memory: {} GB",
+        peak_memory as f32 / (1024.0 * 1024.0 * 1024.0)
     );
-
-    let usage_before = memory_stats().unwrap();
 
     // Load the proving key and verifying key from the files.
     let pk_bytes = std::fs::read("pk.bin").expect("Unable to read file");
     let pk: sp1_sdk::SP1ProvingKey = bincode::deserialize(&pk_bytes).unwrap();
-    // Generate the proof
-    let _ = client
-        .prove(&pk, &stdin)
-        .run()
-        .expect("failed to generate proof");
 
-    let usage_after = memory_stats().unwrap();
+    // Generate the proof
+    let (_, peak_memory) = measure_peak_memory(|| {
+        client
+            .prove(&pk, &stdin)
+            .run()
+            .expect("failed to generate proof")
+    });
 
     println!(
-        "Proving memory usage: {} GB resident | {} GB virt",
-        (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0 * 1024.0),
-        (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0 * 1024.0)
+        "Proving peak memory: {} GB",
+        peak_memory as f32 / (1024.0 * 1024.0 * 1024.0),
     );
 }
