b70803b218d52c90d6ec1807d12a0a0c4fd77a3c
guorong009 <dgr009@pse.dev>
2025-07-24 16:36:21 +0800

    feat: replace "memory_stats" with peak memory measure in binius

--- binius/src/bin/measure.rs
+++ binius/src/bin/measure.rs
@@ -1,28 +1,24 @@
 use anyhow::Error;
 use binius::bench::{prove, sha256_no_lookup_prepare, verify};
-use memory_stats::memory_stats;
+use utils::bench::measure_peak_memory;
 
 fn main() -> Result<(), Error> {
     let allocator = bumpalo::Bump::new();
 
-    let usage_before = memory_stats().unwrap();
-    let (constraint_system, args, witness, backend) = sha256_no_lookup_prepare(&allocator);
-    let usage_after = memory_stats().unwrap();
+    let ((constraint_system, args, witness, backend), peak_memory) =
+        measure_peak_memory(|| sha256_no_lookup_prepare(&allocator));
 
     println!(
-        "Preprocessing memory usage: {} GB resident | {} GB virt",
-        (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0 * 1024.0),
-        (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0 * 1024.0)
+        "Preprocessing peak memory: {} GB",
+        peak_memory as f32 / (1024.0 * 1024.0 * 1024.0),
     );
 
-    let usage_before = memory_stats().unwrap();
-    let (cs, args, proof) = prove(constraint_system, args, witness, backend);
-    let usage_after = memory_stats().unwrap();
+    let ((cs, args, proof), peak_memory) =
+        measure_peak_memory(|| prove(constraint_system, args, witness, backend));
 
     println!(
-        "Proving memory usage: {} GB resident | {} GB virt",
-        (usage_after.physical_mem - usage_before.physical_mem) as f32 / (1024.0 * 1024.0 * 1024.0),
-        (usage_after.virtual_mem - usage_before.virtual_mem) as f32 / (1024.0 * 1024.0 * 1024.0)
+        "Proving peak memory: {} GB",
+        peak_memory as f32 / (1024.0 * 1024.0 * 1024.0)
     );
 
     verify(cs, args, proof);
