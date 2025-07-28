1403b524c08d74923ced3659fc67952826f592f1
Brechy <brechy.web3@gmail.com>
2025-07-28 15:18:05 -0300

    fixes

--- provekit/src/setup.rs
+++
@@ -1,4 +0,0 @@
-// TODO: Merge hash input gen and bench sha256 mods in this file
-// This file is supposed to handle the setup for the circuit files.
-// So random input generations, file parsing into the prover.toml files
-// Evaluate if we need to compile the circuits before benchmarking.
