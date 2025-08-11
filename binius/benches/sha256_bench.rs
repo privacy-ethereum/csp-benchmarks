7c83b6dbc469c58a7692bbf02107788067f14dfe
guorong009 <dgr009@pse.dev>
2025-08-11 21:31:40 +0800

    ref: merge bench and ram measure in binius

--- binius/benches/sha256_bench.rs
+++ binius/benches/sha256_bench.rs
@@ -1,9 +1,26 @@
 // Copyright 2024-2025 Irreducible Inc.
 
 use binius::bench::{prove, sha256_no_lookup_prepare, sha256_with_lookup_prepare, verify};
+use binius_utils::SerializeBytes;
 use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
+use utils::{
+    bench::{SubMetrics, display_submetrics, measure_peak_memory, write_json_submetrics},
+    metadata::SHA2_INPUTS,
+};
 
 fn sha256_no_lookup(c: &mut Criterion) {
+    let mut all_metrics = Vec::new();
+
+    for &num_byte in SHA2_INPUTS.iter() {
+        let metrics = sha2_binius_no_lookup_submetrics(num_byte);
+        all_metrics.push(metrics);
+    }
+
+    println!("{}", display_submetrics(&all_metrics));
+
+    let json_path = "sha2_binius_no_lookup_submetrics.json";
+    write_json_submetrics(json_path, &all_metrics[0]);
+
     let mut group = c.benchmark_group("sha256_no_lookup");
     group.sample_size(10);
     let allocator = bumpalo::Bump::new();
@@ -35,6 +52,18 @@ fn sha256_no_lookup(c: &mut Criterion) {
 }
 
 fn sha256_with_lookup(c: &mut Criterion) {
+    let mut all_metrics = Vec::new();
+
+    for &num_byte in SHA2_INPUTS.iter() {
+        let metrics = sha2_binius_with_lookup_submetrics(num_byte);
+        all_metrics.push(metrics);
+    }
+
+    println!("{}", display_submetrics(&all_metrics));
+
+    let json_path = "sha2_binius_lookup_submetrics.json";
+    write_json_submetrics(json_path, &all_metrics[0]);
+
     let mut group = c.benchmark_group("sha256_with_lookup");
     group.sample_size(10);
     let allocator = bumpalo::Bump::new();
@@ -67,3 +96,49 @@ fn sha256_with_lookup(c: &mut Criterion) {
 
 criterion_main!(sha256);
 criterion_group!(sha256, sha256_no_lookup, sha256_with_lookup);
+
+fn sha2_binius_no_lookup_submetrics(num_bytes: usize) -> SubMetrics {
+    let mut metrics = SubMetrics::new(num_bytes);
+
+    let allocator = bumpalo::Bump::new();
+
+    let ((constraint_system, args, witness, backend), peak_memory) =
+        measure_peak_memory(|| sha256_no_lookup_prepare(&allocator));
+    metrics.preprocessing_peak_memory = peak_memory;
+
+    let mut buffer: Vec<u8> = Vec::new();
+    constraint_system
+        .serialize(&mut buffer, binius_utils::SerializationMode::CanonicalTower)
+        .expect("Failed to serialize constraint system");
+    metrics.preprocessing_size = buffer.len();
+
+    let ((_, _, proof), peak_memory) =
+        measure_peak_memory(|| prove(constraint_system, args, witness, backend));
+    metrics.proving_peak_memory = peak_memory;
+    metrics.proof_size = proof.get_proof_size();
+
+    metrics
+}
+
+fn sha2_binius_with_lookup_submetrics(num_bytes: usize) -> SubMetrics {
+    let mut metrics = SubMetrics::new(num_bytes);
+
+    let allocator = bumpalo::Bump::new();
+
+    let ((constraint_system, args, witness, backend), peak_memory) =
+        measure_peak_memory(|| sha256_with_lookup_prepare(&allocator));
+    metrics.preprocessing_peak_memory = peak_memory;
+
+    let mut buffer: Vec<u8> = Vec::new();
+    constraint_system
+        .serialize(&mut buffer, binius_utils::SerializationMode::CanonicalTower)
+        .expect("Failed to serialize constraint system");
+    metrics.preprocessing_size = buffer.len();
+
+    let ((_, _, proof), peak_memory) =
+        measure_peak_memory(|| prove(constraint_system, args, witness, backend));
+    metrics.proving_peak_memory = peak_memory;
+    metrics.proof_size = proof.get_proof_size();
+
+    metrics
+}
