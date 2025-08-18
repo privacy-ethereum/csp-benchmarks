ef4019b0f6e635e72264c604a765fe40b922a6fd
guorong009 <dgr009@pse.dev>
2025-08-18 18:26:04 +0800

    ref: add memory measure part in provekit

--- provekit/README.md
+++ provekit/README.md
@@ -9,8 +9,14 @@ This benchmark code is using the ProveKit of World Foundation(https://github.com
 cargo bench
 ```
 
-Measure RAM footprint:
+Measure some metrics(e.g. proof size):
 
 ```bash
 cargo run -r --bin provekit-measure
+```
+
+Measure RAM footprint:
+```bash
+chmod +x ../measure_mem_avg.sh
+../measure_mem_avg.sh --json sha2_provekit_mem_report.json -- cargo r -r --bin provekit-measure-mem
 ```
---
+++ provekit/src/bin/measure_mem.rs
@@ -0,0 +1,23 @@
+use provekit::{ProvekitSha256Benchmark, WORKSPACE_ROOT};
+use std::path::PathBuf;
+
+const INPUT_EXPONENTS: [u32; 1] = [11];
+
+fn main() {
+    let bench_harness = ProvekitSha256Benchmark::new(&INPUT_EXPONENTS);
+
+    let mut all_metrics = Vec::new();
+
+    for &exp in INPUT_EXPONENTS.iter() {
+        let package_name = format!("sha256_bench_2e{exp}");
+        let circuit_path = PathBuf::from(WORKSPACE_ROOT)
+            .join("target")
+            .join(format!("{package_name}.json"));
+        let toml_path = PathBuf::from(WORKSPACE_ROOT)
+            .join("circuits/hash/sha256-provekit")
+            .join(format!("sha256-bench-2e{exp}"))
+            .join("Prover.toml");
+
+        let _proof= bench_harness.run_prove(exp);
+    }
+}
