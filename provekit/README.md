f953a79da0a7d6e52bd1f1be135dc75c9cd21036
guorong009 <dgr009@pse.dev>
2025-08-19 18:02:08 +0800

    ref: move submetrics measure to criterion bench in provekit

--- provekit/README.md
+++ provekit/README.md
@@ -9,13 +9,8 @@ This benchmark code is using the ProveKit of World Foundation(https://github.com
 cargo bench
 ```
 
-Measure some metrics(e.g. proof size):
-
-```bash
-cargo run -r --bin provekit-measure
-```
-
 Measure RAM footprint:
+
 ```bash
 chmod +x ../measure_mem_avg.sh
 ../measure_mem_avg.sh --json sha2_provekit_mem_report.json -- cargo r -r --bin provekit-measure-mem
--- provekit/benches/prove_verify.rs
+++ provekit/benches/prove_verify.rs
@@ -1,9 +1,17 @@
 use criterion::{BatchSize, Criterion, black_box, criterion_group, criterion_main};
-use provekit::ProvekitSha256Benchmark;
+use provekit::{ProvekitSha256Benchmark, WORKSPACE_ROOT};
+use std::path::PathBuf;
+use utils::bench::{SubMetrics, display_submetrics, write_json_submetrics};
 
 const INPUT_EXPONENTS: [u32; 1] = [11];
 
 fn sha256_benchmarks(c: &mut Criterion) {
+    // Measure the SubMetrics
+    let metrics = sha256_submetrics();
+    let json_file: &'static str = "sha2_provekit_submetrics.json";
+    write_json_submetrics(json_file, &metrics[0]);
+
+    // Run the benchmarks
     let bench_harness = ProvekitSha256Benchmark::new(&INPUT_EXPONENTS);
     let mut group = c.benchmark_group("SHA256 Prove & Verify");
     group.sample_size(10);
@@ -33,3 +41,36 @@ fn sha256_benchmarks(c: &mut Criterion) {
 
 criterion_group!(benches, sha256_benchmarks);
 criterion_main!(benches);
+
+fn sha256_submetrics() -> Vec<SubMetrics> {
+    let bench_harness = ProvekitSha256Benchmark::new(&INPUT_EXPONENTS);
+
+    let mut all_metrics = Vec::new();
+
+    for &exp in INPUT_EXPONENTS.iter() {
+        let mut metrics = SubMetrics::new(1 << exp);
+
+        let package_name = format!("sha256_bench_2e{exp}");
+        let circuit_path = PathBuf::from(WORKSPACE_ROOT)
+            .join("target")
+            .join(format!("{package_name}.json"));
+        let toml_path = PathBuf::from(WORKSPACE_ROOT)
+            .join("circuits/hash/sha256-provekit")
+            .join(format!("sha256-bench-2e{exp}"))
+            .join("Prover.toml");
+
+        metrics.preprocessing_size = std::fs::metadata(circuit_path)
+            .map(|m| m.len())
+            .unwrap_or(0) as usize
+            + std::fs::metadata(toml_path).map(|m| m.len()).unwrap_or(0) as usize;
+
+        let proof = bench_harness.run_prove(exp);
+        metrics.proof_size = proof.whir_r1cs_proof.transcript.len();
+
+        all_metrics.push(metrics);
+    }
+
+    println!("{}", display_submetrics(&all_metrics));
+
+    all_metrics
+}
