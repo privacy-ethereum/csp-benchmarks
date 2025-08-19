f698774fb6f8f26b9d78fd90d434bf167ae100f6
guorong009 <dgr009@pse.dev>
2025-08-19 19:59:12 +0800

    chore: remove unused code in provekit measure_mem

--- provekit/src/bin/measure_mem.rs
+++ provekit/src/bin/measure_mem.rs
@@ -7,15 +7,6 @@ fn main() {
     let bench_harness = ProvekitSha256Benchmark::new(&INPUT_SIZES);
 
     for &size in INPUT_SIZES.iter() {
-        let package_name = format!("sha256_bench_{size}");
-        let circuit_path = PathBuf::from(WORKSPACE_ROOT)
-            .join("target")
-            .join(format!("{package_name}.json"));
-        let toml_path = PathBuf::from(WORKSPACE_ROOT)
-            .join("circuits/hash/sha256-provekit")
-            .join(format!("sha256-bench-{size}"))
-            .join("Prover.toml");
-
         let _proof = bench_harness.run_prove(size);
     }
 }
