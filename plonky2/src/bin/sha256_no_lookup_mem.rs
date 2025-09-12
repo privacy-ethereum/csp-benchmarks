07e6c8d997bd37e2171b67cdf42a05be92f0e314
Alex Kuzmin <alex.kuzmin@proton.me>
2025-09-12 10:18:24 +0800

    Refactor Plonky2SHA-256 RAM measurement function to accept input size argument

--- plonky2/src/bin/sha256_no_lookup_mem.rs
+++ plonky2/src/bin/sha256_no_lookup_mem.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use plonky2_sha256::bench::{prove, sha256_no_lookup_prepare};
+use plonky2_sha256::bench::{prove, sha256_prepare};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -14,8 +14,7 @@ fn main() {
     sha256_no_lookup_mem(args.input_size);
 }
 
-// TODO: variable input size
-fn sha256_no_lookup_mem(_input_size: usize) {
-    let (data, pw) = sha256_no_lookup_prepare();
+fn sha256_no_lookup_mem(input_size: usize) {
+    let (data, pw) = sha256_prepare(input_size);
     let _proof = prove(&data.prover_data(), pw);
 }
