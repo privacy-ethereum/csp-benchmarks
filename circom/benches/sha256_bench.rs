4d754980b9f289e04d1819d0b8867d93c11d831e
guorong009 <dgr009@pse.dev>
2025-10-01 00:29:24 +0800

    feat: implement "preprocessing_size" measurement

--- circom/benches/sha256_bench.rs
+++ circom/benches/sha256_bench.rs
@@ -15,6 +15,31 @@ utils::define_benchmark_harness!(
     |(_witness_fn, _input_str, zkey_path), proof| {
         circom::verify(proof.clone(), zkey_path.clone())
     },
-    |(_witness_fn, _input_str, _zkey_path)| { 0 },
+    |(_witness_fn, _input_str, zkey_path)| {
+        // NOTE: We assume that the dir which includes "[circuit].zkey" also contains the files 
+        //       needed for witness generation("[circuit].cpp", "[circuit].dat" files).
+        sum_file_sizes_in_the_dir(zkey_path).expect("Unable to compute preprocessing size")
+    },
     |_proof: &CircomProof| 0
 );
+
+fn sum_file_sizes_in_the_dir(file_path: String) -> std::io::Result<usize> {
+    // Get the parent directory
+    let dir = std::path::Path::new(&file_path)
+        .parent()
+        .expect("File should have a parent directory");
+
+    // Sum file sizes in that directory
+    let mut total_size: usize = 0;
+
+    for entry in std::fs::read_dir(dir)? {
+        let entry = entry?;
+        let metadata = entry.metadata()?;
+
+        if metadata.is_file() {
+            total_size += metadata.len() as usize;
+        }
+    }
+
+    Ok(total_size)
+}
