4c26b32edf9f4916842827de62a0a7b4d3a5c511
guorong009 <dgr009@pse.dev>
2025-10-13 21:59:40 +0800

    chore: set nargo version as latest

--- barretenberg/osx_local_setup.sh
+++ barretenberg/osx_local_setup.sh
@@ -38,7 +38,7 @@ else
     # Unknown shell: you can fallback or warn
     echo "Warning: Unknown shell, cannot source rc file automatically" >&2
 fi
-noirup --version 1.0.0-beta.11
+noirup --version 1.0.0-beta.13
 
 # -----------------------
 # Install Barretenberg(bbup)
