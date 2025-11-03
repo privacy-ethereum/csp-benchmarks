c4989c6660dab06bcf47c1ab4888d1a9e2aed9a6
Alex Kuzmin <alex.kuzmin@ethereum.org>
2025-11-03 16:11:29 +0800

    feat: report additional properties for Polyhedra Expander

--- polyhedra-expander/benches/prove_verify.rs
+++ polyhedra-expander/benches/prove_verify.rs
@@ -3,14 +3,26 @@ use sha256_expander_benchmark::bench::get_constraints;
 use sha256_expander_benchmark::bench::prepare;
 use sha256_expander_benchmark::bench::prove;
 use sha256_expander_benchmark::bench::verify;
-use utils::harness::ProvingSystem;
+use utils::harness::{AuditStatus, ProvingSystem};
 
 utils::define_benchmark_harness!(
     BenchTarget::Sha256,
     ProvingSystem::Expander,
     None,
     "sha256_mem_expander",
-    utils::harness::BenchProperties::default(),
+    utils::harness::BenchProperties {
+        proving_system: Some("Libra".to_string()),
+        field_curve: Some("M31".to_string()), // See ./polyhedra-expander/src/bench.rs
+        iop: Some("GKR".to_string()),         // https://eprint.iacr.org/2019/317
+        pcs: Some("Orion".to_string()),       // See ./polyhedra-expander/src/bench.rs
+        arithm: Some("M31".to_string()),      // See ./polyhedra-expander/src/bench.rs
+        is_zk: Some(false),
+        security_bits: Some(128), // https://github.com/PolyhedraZK/Expander/blob/main/poly_commit/src/lib.rs#L6
+        is_pq: Some(true),        // Hash-based PCS (https://eprint.iacr.org/2022/1010.pdf)
+        is_maintained: Some(true),
+        is_audited: Some(AuditStatus::NotAudited),
+        isa: None,
+    },
     {
         let universe = MPIConfig::init().expect("Failed to initialize MPI");
         let world = universe.world();
