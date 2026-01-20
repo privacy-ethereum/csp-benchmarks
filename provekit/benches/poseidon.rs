c67176e6b3522b4240dd490a4c10ecd2d67370bd
brech1 <113475764+brech1@users.noreply.github.com>
2026-01-19 22:43:08 -0300

    feat(provekit): add Poseidon benchmarks

---
+++ provekit/benches/poseidon.rs
@@ -0,0 +1,18 @@
+use provekit::{PROVEKIT_PROPS, prepare_poseidon, preprocessing_size, prove, verify};
+use utils::harness::ProvingSystem;
+
+utils::define_benchmark_harness!(
+    BenchTarget::Poseidon,
+    ProvingSystem::Provekit,
+    None,
+    "poseidon_mem_provekit",
+    PROVEKIT_PROPS,
+    prepare_poseidon,
+    |(proof_scheme, _, _)| { proof_scheme.r1cs.num_constraints() },
+    |(proof_scheme, toml_path, _)| { prove(proof_scheme, toml_path) },
+    |(proof_scheme, _, _), proof| {
+        verify(proof, proof_scheme).unwrap();
+    },
+    |(_, _, circuit_path)| { preprocessing_size(circuit_path) },
+    |proof| { proof.whir_r1cs_proof.transcript.len() }
+);
